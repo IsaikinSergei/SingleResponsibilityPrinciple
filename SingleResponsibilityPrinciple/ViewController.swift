c848dfd94e525dc4d49b6da5b4b8cf59a464b225
IsaikinSergei <akinfest007@gmail.com>
2021-03-17 12:39:16 +0400

    Initial Commit

---
+++ SingleResponsibilityPrinciple/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SingleResponsibilityPrinciple
+//
+//  Created by Sergei Isaikin on 17.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
