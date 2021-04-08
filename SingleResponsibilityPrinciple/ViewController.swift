38a44f01845039e0e569d7d64e03ca78364e8cdc
IsaikinSergei <akinfest007@gmail.com>
2021-04-08 12:57:38 +0400

    initial commit

--- SingleResponsibilityPrinciple/ViewController.swift
+++ SingleResponsibilityPrinciple/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // элементы пользовательского интерфейса
+    @IBOutlet weak var myTextField: UITextField!
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var saveButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        saveButton.layer.cornerRadius = saveButton.frame.width / 2
     }
 
+    // обработка имени
+    
+    func changeName() {
+        guard let name = textLabel.text, name != "" else {
+            showAlert()
+            return
+        }
+        print("Your name: \(name)")
+    }
+    
+    // отображение интерфейса
+    
+    func showAlert() {
+        let alert = UIAlertController(title: "WARNING", message: "Your name can't be empty", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
 
+    // взаимодействие пользователя с интерфейсом
+    
+    @IBAction func changeLabel(_ sender: Any) {
+        textLabel.text = myTextField.text
+    }
+    
+    @IBAction func saveButtonTapped(_ sender: Any) {
+        changeName()
+    }
+    
 }
 
